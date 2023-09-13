171e724a947c54fec345f2b32cf7e23b4d7d9237
gkrankov <krankovics.gabor@gmail.com>
2023-09-13 21:06:19 +0200

    Fixed API Key issue

---
+++ Weather Forecast app/weatherforecast.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+# Replace with your own OpenWeatherMap API key
+api_key = a8391b51d11165580259fd268ef70e1a
+
+def get_weather(city_name):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city_name,
+        "appid": api_key,
+        "units": "metric"  # You can change units to "imperial" for Fahrenheit
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        return None
+
+def display_weather(weather_data):
+    if weather_data:
+        print(f"Weather in {weather_data['name']}, {weather_data['sys']['country']}:")
+        print(f"Temperature: {weather_data['main']['temp']}°C")
+        print(f"Description: {weather_data['weather'][0]['description']}")
+        print(f"Humidity: {weather_data['main']['humidity']}%")
+        print(f"Wind Speed: {weather_data['wind']['speed']} m/s")
+    else:
+        print("Weather data not available.")
+
+if __name__ == "__main__":
+    city_name = input("Enter the name of a city: ")
+    weather_data = get_weather(city_name)
+    display_weather(weather_data)
